e39d40d80b04aa61dae3c51097dac40b28fc75f6
rushdihijab <Rushdihijab@gmail.com>
2022-04-25 15:30:25 -0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+myName = 'Hello World';
+alert(myName);
+let favoriteFood = 'apple';
+document.write(favoriteFood);
+//let size = 100;
+//let doubleSize = size * 2;
+//let minSize = (doubleSize * 2) - (size / 2);
+//document.write(minSize);
