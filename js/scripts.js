0664115cb8c24c032d102edda35bcbbd67502445
rushdihijab <Rushdihijab@gmail.com>
2022-05-11 18:35:30 -0400

    add normalize to css

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
-let pokemonList =[
-  {name: "Bulbasaur", height: 7, types:['grass', 'poison'] },
-  {name: "Ivysaur", height: 10, types:['monster', 'grass'] },
-  {name: "Charmander", height: 6, types:['monster', 'dragon'] },
-];
+const pokemonList = [{
+	name: 'Bulbasaur',
+	height: 7,
+	types: ['grass', 'poison'],
+}, {
+	name: 'Ivysaur',
+	height: 10,
+	types: ['monster', 'grass'],
+}, {
+	name: 'Charmander',
+	height: 6,
+	types: ['monster', 'dragon'],
+}];
+for (let i = 0; i < pokemonList.length; i++) {
+	if (pokemonList[i].height < 10 && pokemonList[i].height > 6) {
+		document.write(pokemonList[i].name + " ( Height: ", pokemonList[i].height + ")" + "<br/>");
+	} else if (pokemonList[i].height <= 6) {
+		document.write(pokemonList[i].name + " ( Height: ", pokemonList[i].height + ")" + "<br/>");
+	} else {
+		document.write(pokemonList[i].name + " (Height: ", pokemonList[i].height + ")" + " -Wow, that’s big!<br/>");
+	}
+}
