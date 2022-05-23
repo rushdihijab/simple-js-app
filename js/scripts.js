bbd4bf6e8bb6e7cbca314ef09c21ce67691106d2
rushdihijab <Rushdihijab@gmail.com>
2022-05-23 15:01:06 -0400

    use the forEach() function rather than the regular for loop and using IIFE
    1. Use a forEach() function instead of the for loop .
    2.  create a new pokemonRepository variable to hold what your IIFE will return, then assign the IIFE to that variable.
    3. return an object with (getAll , add) functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,36 @@
-const pokemonList = [{
-	name: 'Bulbasaur',
-	height: 7,
-	types: ['grass', 'poison'],
-}, {
-	name: 'Ivysaur',
-	height: 10,
-	types: ['monster', 'grass'],
-}, {
-	name: 'Charmander',
-	height: 6,
-	types: ['monster', 'dragon'],
-}];
-for (let i = 0; i < pokemonList.length; i++) {
-	if (pokemonList[i].height < 10 && pokemonList[i].height > 6) {
-		document.write(pokemonList[i].name + " ( Height: ", pokemonList[i].height + ")" + "<br/>");
-	} else if (pokemonList[i].height <= 6) {
-		document.write(pokemonList[i].name + " ( Height: ", pokemonList[i].height + ")" + "<br/>");
-	} else {
-		document.write(pokemonList[i].name + " (Height: ", pokemonList[i].height + ")" + " -Wow, that’s big!<br/>");
-	}
-}
+let pokemonRepository = (function() {
+	let pokemonList = [{
+		name: 'Bulbasaur',
+		height: 7,
+		types: ['grass', 'poison']
+	}, {
+		name: 'Ivysaur',
+		height: 10,
+		types: ['monster', 'grass']
+	}, {
+		name: 'Charmander',
+		height: 6,
+		types: ['monster', 'dragon']
+	}];
+
+	Object.keys(pokemonList).forEach(function(i) {
+		console.log(pokemonList[i]);
+	});
+
+	return {
+		add: function(pokemon) {
+			pokemonList.push(pokemon);
+		},
+		getAll: function() {
+			return pokemonList;
+		}
+	};
+})();
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({
+	name: 'Butterfly',
+	height: 3,
+	types: ['bug', 'flying']
+});
+console.log();
+(pokemonRepository.getAll());
