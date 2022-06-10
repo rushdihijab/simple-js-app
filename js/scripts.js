d4c3e0d57bfa845a547b392c697a8436318a0f1c
rushdihijab <Rushdihijab@gmail.com>
2022-06-10 13:58:23 -0400

    replace the static list of Pokémon with the complete list of Pokémon loaded from the free Pokémon API.
    1. In the “scripts.js” file of your project, remove the array of Pokémon objects and replace it with an empty array.
    2. Add function(s) inside your data repository to load data from an external source.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,7 @@
 let pokemonRepository = (function() {
-	let repository = [{
-		name: 'Bulbasaur',
-		height: 7,
-		types: ['grass', 'poison']
-	}, {
-		name: 'Ivysaur',
-		height: 10,
-		types: ['monster', 'grass']
-	}, {
-		name: "Charizard",
-		height: 1.7,
-		types: ['fire', 'flying'],
-	}, {
-		name: "Squirtle",
-		height: 1,
-		types: ["water"],
-	}, {
-		name: 'Charmander',
-		height: 6,
-		types: ['monster', 'dragon']
-	}];
+	let repository = [];
+	let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
 
 	function add(pokemon) {
 		repository.push(pokemon);
@@ -38,30 +20,64 @@ let pokemonRepository = (function() {
 		listpokemon.appendChild(button);
 		pokemonList.appendChild(listpokemon);
 		addEvent(button, pokemon);
-  }
-  function addEvent(button,pokemon) {
-    button.addEventListener('click', function() {
-      showDetails(pokemon);
-    });
-  }
-    function showDetails(pokemon) {
-        console.log(pokemon);
 	}
+
+	function addEvent(button, pokemon) {
+		button.addEventListener('click', function() {
+			showDetails(pokemon);
+		});
+	}
+
+	function loadList() {
+		return fetch(apiUrl).then(function(response) {
+			return response.json();
+		}).then(function(json) {
+			json.results.forEach(function(item) {
+				let pokemon = {
+					name: item.name,
+					detailsUrl: item.url
+				};
+				add(pokemon);
+				console.log(pokemon);
+			});
+		}).catch(function(e) {
+			console.error(e);
+		});
+	}
+
+	function loadDetails(item) {
+		let url = item.detailsUrl;
+		return fetch(url).then(function(response) {
+			return response.json();
+		}).then(function(details) {
+			// Now we add the details to the item
+			item.imageUrl = details.sprites.front_default;
+			item.height = details.height;
+			item.types = details.types;
+		}).catch(function(e) {
+			console.error(e);
+		});
+	}
+
+	function showDetails(item) {
+		pokemonRepository.loadDetails(item).then(function() {
+			console.log(item);
+		});
+	}
+
 	return {
 		add: add,
 		getAll: getAll,
-		addListItem: addListItem
+		addListItem: addListItem,
+		loadList: loadList,
+		loadDetails: loadDetails,
+		showDetails: showDetails
 	};
 })();
 
-pokemonRepository.add({
-	name: "Pikachu",
-	height: 0.3,
-	types: ["electric"]
-});
 
-console.log(pokemonRepository.getAll());
-
-pokemonRepository.getAll().forEach(function(pokemon) {
-	pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+	pokemonRepository.getAll().forEach(function(pokemon) {
+		pokemonRepository.addListItem(pokemon);
+	});
 });
